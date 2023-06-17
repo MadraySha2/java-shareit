ab5ae1cd1fc789c2efb6f466281ea571c9ac0e4e
Volodia <shonkatler@gmail.com>
2023-06-17 14:39:23 +0300

    КодСтайл

---
+++ server/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -0,0 +1,53 @@
+package ru.practicum.shareit.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.comment.CommentDto;
+
+import javax.xml.bind.ValidationException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+@RequiredArgsConstructor
+public class ItemController {
+
+
+    private final ItemService itemService;
+
+    @GetMapping
+    public List<ItemDto> getItems(@RequestHeader("X-Sharer-User-Id") Long id) {
+        return itemService.getItems(id);
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDto getItemById(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable Long itemId) {
+        return itemService.getItemById(userId, itemId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(@RequestParam @DefaultValue(value = " ") String text,
+                                     @RequestParam(defaultValue = "0") int from,
+                                     @RequestParam(defaultValue = "10") int size) throws ValidationException {
+        return itemService.searchItems(text, from, size);
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(@RequestHeader("X-Sharer-User-Id") @DefaultValue(value = "0L") Long id,
+                              @RequestBody ItemDto itemDto, @PathVariable Long itemId) {
+        return itemService.updateItem(id, itemDto, itemId);
+    }
+
+    @PostMapping
+    public ItemDto addItem(@RequestHeader("X-Sharer-User-Id") Long id, @RequestBody ItemDto itemDto) {
+        return itemService.addItem(id, itemDto);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentDto addComment(@RequestHeader("X-Sharer-User-Id") Long id,
+                                 @PathVariable Long itemId, @RequestBody CommentDto commentDto) {
+        return itemService.addComment(id, itemId, commentDto);
+    }
+
+}
