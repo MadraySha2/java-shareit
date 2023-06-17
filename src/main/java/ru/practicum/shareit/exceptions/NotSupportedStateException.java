0eeee90eba890ad8db2d7f701ee12d88577d2ea9
Volodia <shonkatler@gmail.com>
2023-06-17 14:37:14 +0300

    КодСтайл

--- src/main/java/ru/practicum/shareit/exceptions/NotSupportedStateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class NotSupportedStateException extends RuntimeException {
-    public NotSupportedStateException(String message) {
-        super(message);
-    }
-}
